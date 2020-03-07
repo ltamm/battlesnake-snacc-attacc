14e1978e01dfbbf835d515a6edb459bdd9bcea6c
ltamm <laura.tammpere@gmail.com>
2020-03-07 11:35:26 -0800

    implemented boundary checking in very bad way: will check directions in set order for board boundary (but will not register snake bodies

--- decider.rb
+++ decider.rb
@@ -7,22 +7,23 @@ class Decider
 
   def initialize(board, player_id)
     @board = board
-    @player_id = player_id
+    @player_id = player_id.to_sym
   end
 
   def clear_path?(direction)
     # blockages checked:
     #   - out of bound array indices of board
-    head = board.snakes[player_id].body[0].body_segment_to_coordinate
+    head = board.snakes[player_id].body[0]
+    head_coordinate = body_segment_to_coordinate head
     case direction
     when :up
-      next_coordinate = Coordinate.new head['x'], head['y'] - 1
+      next_coordinate = Coordinate.new head_coordinate['x'], head_coordinate['y'] - 1
     when :right
-      next_coordinate = Coordinate.new head['x'] + 1, head['y']
+      next_coordinate = Coordinate.new head_coordinate['x'] + 1, head_coordinate['y']
     when :left
-      next_coordinate = Coordinate.new head['x'] - 1, head['y']
+      next_coordinate = Coordinate.new head_coordinate['x'] - 1, head_coordinate['y']
     when :down
-      next_coordinate = Coordinate.new head['x'], head['y'] - 1
+      next_coordinate = Coordinate.new head_coordinate['x'], head_coordinate['y'] + 1
     end
     !(out_of_bounds? next_coordinate)
   end
@@ -32,10 +33,10 @@ class Decider
   end
 
   def out_of_bounds?(coordinate)
-    coordinate.x.negative?        ||
-      coordinate.x >= board.width ||
-      coordiante.y.negative?      ||
-      coordinate.y >= board.height
+    coordinate.x.negative?          ||
+      coordinate.x == board.width   ||
+      coordinate.y.negative?        ||
+      coordinate.y == board.height
   end
 
   def decide
--- spec/decider_spec.rb
+++ spec/decider_spec.rb
@@ -5,24 +5,36 @@ require_relative '../battlesnake_server.rb'
 
 describe Decider do
   describe 'decide' do
-    before(:each) do
-      board = double('BattleSnake::Board')
-      @decider = Decider.new(board, 'test_id')
-    end
-    context 'if no direction is clear' do
+    context 'if up is clear' do
       it 'returns move up' do
+        test_snake = BattleSnake::Snake.new 'name', 100, [ { 'x' => 0, 'y' => 1 } ]
+        board = BattleSnake::Board.new 3, 1, [], { 'test_id': test_snake }
+        @decider = Decider.new(board, 'test_id')
+        expect(@decider.decide).to eq('up')
       end
     end
-    context 'if down is clear' do
+    context 'if down is clear (and up is not)' do
       it 'returns move down' do
+        test_snake = BattleSnake::Snake.new 'name', 100, [ { 'x' => 0, 'y' => 0 } ]
+        board = BattleSnake::Board.new 3, 1, [], { 'test_id': test_snake }
+        @decider = Decider.new(board, 'test_id')
+        expect(@decider.decide).to eq('down')
       end
     end
-    context 'if left is clear' do
+    context 'if left is clear (and up and down are not)' do
       it 'returns move left' do
+        test_snake = BattleSnake::Snake.new 'name', 100, [ { 'x' => 1, 'y' => 0 } ]
+        board = BattleSnake::Board.new 1, 3, [], { 'test_id': test_snake }
+        @decider = Decider.new(board, 'test_id')
+        expect(@decider.decide).to eq('left')
       end
     end
-    context 'if right is clear' do
+    context 'if right is clear (and nothing else is)' do
       it 'returns move right' do
+        test_snake = BattleSnake::Snake.new 'name', 100, [ { 'x' => 0, 'y' => 0 } ]
+        board = BattleSnake::Board.new 1, 3, [], { 'test_id': test_snake }
+        @decider = Decider.new(board, 'test_id')
+        expect(@decider.decide).to eq('right')
       end
     end
   end
