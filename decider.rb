67d0fa0eefbdfce53220cabb9404d1fced24d69e
ltamm <laura.tammpere@gmail.com>
2020-03-07 10:58:34 -0800

    added out of bounds checking for initial "AI"

--- decider.rb
+++ decider.rb
@@ -11,10 +11,31 @@ class Decider
   end
 
   def clear_path?(direction)
-    # blockages include out of bound array indices of board
-    # and snake bodies
-    puts "TODO: check for blockages in direction: #{direction}"
-    true
+    # blockages checked:
+    #   - out of bound array indices of board
+    head = board.snakes[player_id].body[0].body_segment_to_coordinate
+    case direction
+    when :up
+      next_coordinate = Coordinate.new head['x'], head['y'] - 1
+    when :right
+      next_coordinate = Coordinate.new head['x'] + 1, head['y']
+    when :left
+      next_coordinate = Coordinate.new head['x'] - 1, head['y']
+    when :down
+      next_coordinate = Coordinate.new head['x'], head['y'] - 1
+    end
+    !(out_of_bounds? next_coordinate)
+  end
+
+  def body_segment_to_coordinate(segment)
+    Coordinate.new segment['x'], segment['y']
+  end
+
+  def out_of_bounds?(coordinate)
+    coordinate.x.negative?        ||
+      coordinate.x >= board.width ||
+      coordiante.y.negative?      ||
+      coordinate.y >= board.height
   end
 
   def decide
@@ -23,7 +44,9 @@ class Decider
     end
 
     # if for some sad reason no direction is clear, it is game over
-    # and we nobly (and arbitrarily) go up
+    # and we arbitrarily go up :(
     'up'
   end
+
+  Coordinate = new Struct(:x, :y)
 end
